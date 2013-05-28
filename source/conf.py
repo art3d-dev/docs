87b751d74ba7c858e2f3fcdc71e48ca2f584bb21
Max Korinets <mkorinets@art3d.ru>
2013-05-28 12:41:00 +0400

    added to faq, CSV

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ART3D'
-copyright = u'2013, Maxim Korinets'
+copyright = u'2013, ART3D Graphics'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
